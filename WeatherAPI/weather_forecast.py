93e8adfbfa22cc259d7c1d2633c5dd7fce3a5760
Jackmastro <giacomma@student.ethz.ch>
2023-08-16 13:22:05 +0200

    check_variables completed based on documentation

--- WeatherAPI/weather_forecast.py
+++ WeatherAPI/weather_forecast.py
@@ -13,26 +13,57 @@ class WeatherForecast():
         # then introduce online position and look some km ahead in the direction of the road (needed roate lat and long)
         pass
 
-    def create_request_url(self, ):
-        url: str = self.api_website
+    # TODO UTC timezone check
+
+    def create_request_url(self, **kwargs):
+        # to remove this function
+        url = f'{self.api_website}key={self.key}&service={self.service}&format={self.format}'
+
+        for key, value in kwargs.items():
+            url += f'&{key}={value}'
 
         return url
+    
+    def check_variables(self, **kwargs):
+        validation_rules = { # horizon, hddctin, hddctout, and cddctout are not included
+            'site_id': (int, None, None),
+            'name': (str, None, None),
+            'latitude': (float, -90.0, 90.0),
+            'longitude': (float, -180.0, 180.0),
+            'azimuth': (int, 0, 360),
+            'inclination': (int, 0, 90),
+            'altitude': (int, None, None)
+        }
 
-    def get_site_add(self, ):
-        # initially only one by one, then with a vector of positions received or saved here
-        action: str = 'siteadd'
+        for key, value in kwargs.items():
+            if key in validation_rules:
+                value_type, min_value, max_value = validation_rules[key]
+
+                if not isinstance(value, value_type):
+                    raise ValueError(f'{key} has to be a {value_type}. Received: {value}')
+
+                if min_value is not None and max_value is not None:
+                    if not (min_value <= value <= max_value):
+                        raise ValueError(f'{key} has to be between {min_value} and {max_value}. Received: {value}')
+
+    def get_site_add(self, name, latitude, longitude, azimuth=0, inclination=0, **kwargs):
+        # initially only one by one, then with a vector of positions received as input
         
 
-"https://mdx.meteotest.ch/api_v1?key=54F773F38E50F4CF562384A44B9948D3&service=solarforecast&action=siteadd&format=json&name=testDarwin&latitude=-12.39828502488282&longitude=130.88590799669225&inclination=0"
+        action: str = 'siteadd'
 
-payload = {}
-headers = {}
+        url = f'{self.api_website}key={self.key}&service={self.service}&action={action}&format={self.format}'
+        
 
-response = requests.request("GET", url, headers=headers, data=payload)
+        "https://mdx.meteotest.ch/api_v1?key=54F773F38E50F4CF562384A44B9948D3&service=solarforecast&action=siteadd&format=json&name=testDarwin&latitude=-12.39828502488282&longitude=130.88590799669225&inclination=0"
 
-print(response.text)
+        payload = {}
+        headers = {}
 
-        pass
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        print(response.text)
+        requests.get(mdx_url)
 
     def get_site_delete(self, ):
         action: str = 'sitedelete'
