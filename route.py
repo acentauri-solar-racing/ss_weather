91f096e26710ef968ba3fd0e290922b0211b03cf
Jackmastro <giacomma@student.ethz.ch>
2023-08-21 13:30:36 +0200

    missing dataframe concatenation

--- route.py
+++ route.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import os
 import constants
-from geopy.distance import geodesic
 from scipy.spatial import KDTree
 
 class Route():
@@ -34,28 +33,57 @@ class Route():
         start_distance = self.route_data.iloc[start_index]['CumDistance']
 
         # Cut data and start the cumulative distance from the new position
-        cut_data = self.route_data.iloc[start_index:].copy()
+        cut_data = self.route_data.iloc[start_index:]
         cut_data['CumDistance'] -= start_distance
+        # Drop old indexing
+        cut_data = cut_data.reset_index(drop=True)
+        # Delete Surface type
+        cut_data = cut_data.drop('Surface', axis=1)
 
         if delta_spacing is None:
+            print(cut_data)
             return cut_data
         else:
             if not isinstance(delta_spacing, int) or (self.min_delta_spacing > delta_spacing > self.max_delta_spacing):
                 raise ValueError(f'{delta_spacing} has to be an integer between {self.min_delta_spacing} and {self.max_delta_spacing}. Received: {delta_spacing}')
 
             # Create a copy of the first row for interpolated data
-            interpolated_data = cut_data.iloc[[0]].copy()
-
-            interp_number = int(cut_data['CumDistance'].max() / delta_spacing)
-            x = np.arange(interp_number + 2) * delta_spacing
+            interpolated_data = cut_data.iloc[[0]]
+            # Find value of interpolation points
+            number_inter_point = int(cut_data['CumDistance'].max() / delta_spacing)
+            # Define the ascending equally-spaced vector
+            x = np.arange(number_inter_point + 2) * delta_spacing
             x[-1] = cut_data['CumDistance'].max()
-
+            # Interpolation points
             xp = cut_data['CumDistance']
 
-            # Interpolate selected columns (excluding 'CumDistance')
+            # Interpolate selected columns
             for column in cut_data.columns:
-                if column != 'CumDistance':
-                    fp = cut_data[column]
-                    interpolated_data[column] = np.interp(x, xp, fp)
+                print(interpolated_data)
 
+                if column == 'Maxspeed':
+                    # Find the indices of x in xp (closest smaller indices)
+                    indices = np.searchsorted(xp, x, side='right') - 1
+                    interpolated_data[column] = cut_data[column].iloc[indices]
+
+                elif column == 'CumDistance':
+                    interpolated_data[column] = pd.Series(x)
+
+                else:
+                    fp = cut_data[column]
+                    interpolated_values = np.interp(x, xp, fp)
+                    # interpolated_data.loc[:, column] = interpolated_values
+                    # interpolated_data[column] = interpolated_values
+                    interpolated_data[column] = pd.Series(interpolated_values)
+            
             return interpolated_data
+        
+route = Route()
+actual_position = {'longitude': 130,
+                   'latitude': -12.4}
+
+delta_spacing = 1000000 # in meters
+# route.get_final_data(actual_position)
+route.get_final_data(actual_position, delta_spacing)
+
+# TODO check all NaN
