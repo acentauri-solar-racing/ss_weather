bca197dab03f0c32563497f7488737360c736ecb
Jackmastro <giacomma@student.ethz.ch>
2023-10-04 17:27:42 +1030

    bounds for the map found

--- plotter.py
+++ plotter.py
@@ -1,52 +1,73 @@
 import folium
+import pandas as pd
 
 class Plotter():
+    """ """
     def __init__(self, route_data) -> None:
         #TODO PER GENERALIZZARLO PRENDERE "PUNTO MEDIO" DEI PUNTI IN ROUTE
 
         # Create a base map centered around Australia
-        self.map = folium.Map(location=[-25.2744, 133.7751], min_zoom=5, max_bounds=True)
+        self.map = folium.Map(location=[-25.2744, 133.7751],
+                              zoom_start=0)
 
         # Define the bounds
-        bounds = [[-35, 125], [-12, 140]]
+        min_lng = route_data['longitude'].min()
+        min_lat = route_data['latitude'].min()
+        max_lng = route_data['longitude'].max()
+        max_lat = route_data['latitude'].max()
+        bounds = [[max_lat, min_lng], [min_lat, max_lng]]
         self.map.fit_bounds(bounds)
 
-    def 
+        self.map.options['maxBounds'] = bounds
+        self.map.options['maxBoundsViscosity'] = 0.9
+        self.map.options['minZoom'] = 5
 
-control_stops_df = route_data.get_control_stops_data
-sites_df = api_requester.get_current_sites
+        folium.PolyLine(
+        locations=route_data[['latitude', 'longitude']].values.tolist(),
+        color="green",
+        weight=4,
+        opacity=1
+        ).add_to(self.map)
 
-# Add the route lines
-rt = route_data.get_route_data
-folium.PolyLine(
-    locations=rt[['latitude', 'longitude']].values.tolist(),
-    color="green",
-    weight=4,
-    opacity=1
-).add_to(map)
+    @property
+    def plot(self) -> None:
+        """ """
+        return self.map
+    
+    def add_weather_sites(self, api_sites:pd.DataFrame) -> None:
+        """ """
+        for _, row in api_sites.iterrows():
+            folium.CircleMarker(
+                location=[row['latitude'], row['longitude']],
+                radius=2,
+                color="blue",
+                fill=True,
+                fill_color="blue",
+                fill_opacity=1,
+                popup=row['name']
+            ).add_to(self.map)
+    
+    def add_control_stops(self, control_stops_df:pd.DataFrame) -> None:
+        """ """
+        for _, row in control_stops_df.iterrows():
+            folium.CircleMarker(
+                location=[row['latitude'], row['longitude']],
+                radius=3,
+                color="orange",
+                fill=True,
+                fill_color="orange",
+                fill_opacity=1,
+                popup=row['location']
+            ).add_to(self.map)
 
-# Add api sites
-for _, row in sites_df.iterrows():
-    folium.CircleMarker(
-        location=[row['latitude'], row['longitude']],
-        radius=2,
-        color="blue",
-        fill=True,
-        fill_color="blue",
-        fill_opacity=1,
-        popup=row['name']
-    ).add_to(map)
-
-# Add control stops
-for _, row in control_stops_df.iterrows():
-    folium.CircleMarker(
-        location=[row['latitude'], row['longitude']],
-        radius=3,
-        color="orange",
-        fill=True,
-        fill_color="orange",
-        fill_opacity=1,
-        popup=row['location']
-    ).add_to(map)
-
-map
+    def add_current_position(self, current_position) -> None:
+        """ """
+        folium.CircleMarker(
+                location=[current_position['latitude'], current_position['longitude']],
+                radius=3,
+                color="red",
+                fill=True,
+                fill_color="red",
+                fill_opacity=1,
+                popup="You"
+            ).add_to(self.map)
